06fc37cc70366667fb4c31d81c54a1c1b6a9f903
tcfplayz <chau123ka89chun10p@gmail.com>
2023-11-20 17:38:09 +0800

    4.1.6 - fixes

--- src/main/java/ml/spmc/smpmod/minecraft/command/AllCommands.java
+++ src/main/java/ml/spmc/smpmod/minecraft/command/AllCommands.java
@@ -8,12 +8,12 @@ import net.minecraft.server.command.ServerCommandSource;
 
 public class AllCommands {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(BalanceCommand.buildCommand());
-        dispatcher.register(WithdrawCommand.buildCommand());
+        //dispatcher.register(BalanceCommand.buildCommand());
+        //dispatcher.register(WithdrawCommand.buildCommand());
 
-        dispatcher.register(DeployCommand.buildCommand());
+        //dispatcher.register(DeployCommand.buildCommand());
         dispatcher.register(SurfaceCommand.buildCommand());
-        dispatcher.register(MyIPCommand.buildCommand());
+        //dispatcher.register(MyIPCommand.buildCommand());
         dispatcher.register(ECCommand.buildCommand());
         dispatcher.register(GarbageCommand.buildCommand());
     }
