5534123bc3488e2d525f1854252076f874b64275
tcfplayz <chau123ka89chun10p@gmail.com>
2023-11-21 16:02:44 +0800

    4.1.6 - refactor

--- src/main/java/ml/spmc/smpmod/minecraft/command/CommandRegister.java
+++ src/main/java/ml/spmc/smpmod/minecraft/command/CommandRegister.java
@@ -1,17 +1,14 @@
 package ml.spmc.smpmod.minecraft.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import ml.spmc.smpmod.minecraft.command.screen.command.ECCommand;
-import ml.spmc.smpmod.minecraft.command.screen.command.GarbageCommand;
 import net.minecraft.server.command.ServerCommandSource;
+
+import ml.spmc.smpmod.minecraft.command.screen.command.*;
 
 public class CommandRegister {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(BalanceCommand.buildCommand());
-        //dispatcher.register(WithdrawCommand.buildCommand());
-        //dispatcher.register(DeployCommand.buildCommand());
         dispatcher.register(SurfaceCommand.buildCommand());
-        //dispatcher.register(MyIPCommand.buildCommand());
         dispatcher.register(ECCommand.buildCommand());
         dispatcher.register(GarbageCommand.buildCommand());
     }
