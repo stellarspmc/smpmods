88aab8103c2da2036362c1b9b3d66383f2301dce
tcfplayz <chau123ka89chun10p@gmail.com>
2026-09-19 06:44:48 +0800

    v7.3pre5 - backport treasure bug

--- src/main/java/spmc/smpmod/mining/TreasureHelper.kt
+++ src/main/java/spmc/smpmod/mining/TreasureHelper.kt
@@ -22,7 +22,6 @@ import spmc.smpmod.mining.ChunkPool.multiplier
 import spmc.smpmod.mining.TreasureSpawner.spawnTreasureContainer
 import spmc.smpmod.utils.checkNotCreative
 import java.util.*
-import kotlin.collections.HashMap
 import kotlin.enums.enumEntries
 import kotlin.jvm.optionals.getOrNull
 import kotlin.math.ln
@@ -62,7 +61,7 @@ object TreasureHelper {
 	    val roll = random.nextDouble()
 	    var cumulative = .0
 	    for (rarity in ItemRarity.entries) {
-		    cumulative += getThreshold(rarity.ordinal)
+		    cumulative += getThreshold(7 - rarity.ordinal)
 		    if (roll <= cumulative) return adjustRarity(rarity)
 	    }
 	    return null
